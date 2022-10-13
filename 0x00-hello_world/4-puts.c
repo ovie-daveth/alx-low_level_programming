64019baaa10dd09a3a78ca1e4c079bea8fd66c47
ovie <oviedavid77@gmail.com>
2022-10-13 07:39:40 -0700

    gcc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/*
+/**
  * Main: Entry
  * return: Always 0
-  */
-
+ */
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
