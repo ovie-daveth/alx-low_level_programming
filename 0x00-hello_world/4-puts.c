bd3affb12d33813820cc3c9723dd3bb764377e8e
ovie <oviedavid77@gmail.com>
2022-10-13 07:02:44 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * Main : Entry
+ * returns0
+*/
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/hi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+	puts("me is here");
+
+	return (0);
+
+}
