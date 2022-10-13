e8fcc424626aadebef28c8faa70463e83dca0225
ovie <oviedavid77@gmail.com>
2022-10-13 10:08:09 -0700

    advnace

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*Main: return 0 */
+int main(void)
+{
+	int a;
+	char b;
+	float c;
+	double e;
+
+	printf("%lu.\n", (unsigned long)sizeof(a));
+	printf("%lu.\n", (unsigned long)sizeof(b));
+	printf("%lu.\n", (unsigned long)sizeof(c));
+	printf("%lu.\n", (unsigned long)sizeof(e));
+	return (0);
+}
