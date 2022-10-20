2bd29d8afbf52c4c6fa6c30cde74044be5807ec7
ovie <oviedavid77@gmail.com>
2022-10-20 06:10:18 -0700

    third

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
