483b6070e3df7645e892f5fbe675e189fcdb5e06
ovie <oviedavid77@gmail.com>
2022-10-20 06:19:08 -0700

    fouirth

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the nubers 0 to 9
+ * Return: 0 as success
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
