5d29d5ba55e66de1c5e9d390c3d23c392594b44c
ovie <oviedavid77@gmail.com>
2022-11-04 05:55:25 -0700

    pushee

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints out the name of the function
+ * @argc: number of argument
+ * @argv: names of argument
+ * Return: 0 always for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("The name of my fuction is %s \n", argv[1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - couts the number of argumet
+ * @argc: number of argument
+ * @argv: arguments passed
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("The number of rgument passed is %d\n", argc);
+	return (0);
+}
